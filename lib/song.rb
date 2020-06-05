80eba04e1f079cf3da0f2a03a98270e357fa1a71
korey ko <koreykendrick1@gmail.com>
2020-06-05 02:00:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
   
   def self.find_or_create_by_name(find_this_song)
-did_i_find_it = self.all.detect {|x| x.name == find_this_song}
+did_i_find_it = self.find_by_name(string_name_the_song)
     if did_i_find_it == nil
       s = self.new 
       s.name = find_this_song
